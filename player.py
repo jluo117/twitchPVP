2425f637d4ff5bbcc2b599181ef9aa4329e20a3f
bball24 <jerry88tan@gmail.com>
2017-10-21 05:57:32 -0700

    Added a timer to do cooldowns for certain chat commands

--- player.py
+++ player.py
@@ -1,18 +1,31 @@
+import time
+used = {}
+freq = 5
 class player:
     def _init_(self,Name):
         self.HP = 1000
         self.counter = 0
         self.Attack = 0
         self.Name = name
+	self.command = None
+	
     def turn(chatInput):
         for chat in chatInput:
             if chat == "attack" or chat == "Attack":
                 self.Attack = self.Attack + 1
+		self.command = "attack"
             if chat == "counter" or chat == "Counter":
                 self.counter = self.counter + 1
+		self.command = "counte"r
             if chat == "heal" or chat == "Heal":
                 self.HP = self.HP + 2
-
+		self.command = "heal"
+	
+    def call_command(command):
+	if(self.command == "heal" or self.command == "counter"):
+	print("Calling command '%s'." % self.command)
+	
+	 
     def Attack(player):
         self.Attack = self.Attack - (player.counter * 2)
         player.hp = player.HP - self.Attack
@@ -22,11 +35,26 @@ class player:
 	self.Attack = self.Attack + 3;
 	self.hp = self.hp * 1.2;
 	self.counter = self.counter * 1.2;
-    def interupt(player):
+    def counter(player):
 	if player.chat == "attack"
 	    player.hp = player.hp - (0.3 * player.attack)
 	    player.attack = 0;
 	else
-	    self.interupt = 0
+	    self.counter = 0
+   def cooldown(self):
+	if(chat == "heal" or chat == "Heal"):
+ 	  print("You have used the command Heal in the last %u seconds." % (chat, freq))
+	else:
+	  print("You have used the command Counter in the last %u seconds" % (chat, freq))
+   def process_command(command)
+	if(
+	   command not in used or
+	   time.time() - used[command] > freq
+	
+):
+	used[command] = time.time()
+ 	call_command(command)
 
+	else:
+		cooldown(command)
 
