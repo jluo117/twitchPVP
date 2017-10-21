064440756c1c808f30b5cc12c03312b5e011f88a
jluo117 <luojames52@gmail.com>
2017-10-21 01:57:59 -0700

    chatbot thread ready

--- chatbot.py
+++ chatbot.py
@@ -11,54 +11,93 @@ or in the "license" file accompanying this file. This file is distributed on an 
 import sys
 import irc.bot
 import requests
-from Game import Game
-from player import player
+from threading import Thread
+
 class TwitchBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, username, client_id, token, channel):
-        self.client_id = client_id
-        self.token = token
-        self.channel = '#' + channel
+    def __init__(self, channelID, callback):
+        self.client_id = 'jjedef9zz8klzeabdrd7earose2qdg'
+        self.token = '9vom3faszykwz2v01okux2cr4gqpo2'
+        self.channel = '#' + channelID
+        self.onMessage = callback
 
         # Get the channel id, we will need this for v5 API calls
-        url = 'https://api.twitch.tv/kraken/users?login=' + channel
-        headers = {'Client-ID': client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        url = 'https://api.twitch.tv/kraken/users?login=' + 'bloodyplayer415'
+        headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
         r = requests.get(url, headers=headers).json()
         self.channel_id = r['users'][0]['_id']
 
         # Create IRC bot connection
         server = 'irc.chat.twitch.tv'
         port = 6667
-        print 'Connecting to ' + server + ' on port ' + str(port) + '...'
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+token)], username, username)
+        print ('Connecting to ' + server + ' on port ' + str(port) + '...')
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+self.token)], 'bloodyplayer415', 'bloodyplayer415')
 
 
+    def on_welcome(self, c, e):
+        print ('Joining ' + self.channel)
+
+        # You must request specific capabilities before you can use them
+        c.cap('REQ', ':twitch.tv/membership')
+        c.cap('REQ', ':twitch.tv/tags')
+        c.cap('REQ', ':twitch.tv/commands')
+        c.join(self.channel)
 
     def on_pubmsg(self, c, e):
 
         # If a chat message starts with an exclamation point, try to run it as a command
         if e.arguments[0][:1] == '!':
             cmd = e.arguments[0].split(' ')[0][1:]
-            print 'Received command: ' + cmd
-            cmd.append
+            print ('Received command: ' + cmd + self.channel)
             self.do_command(e, cmd)
+            self.onMessage(self.channel, cmd)
         return
 
+    def do_command(self, e, cmd):
+        c = self.connection
+
+        # Poll the API to get current game.
+        if cmd == "game":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' is currently playing ' + r['game'])
+
+        # Poll the API the get the current status of the stream
+        elif cmd == "title":
+            url = 'https://api.twitch.tv/kraken/channels/' + self.channel_id
+            headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+            r = requests.get(url, headers=headers).json()
+            c.privmsg(self.channel, r['display_name'] + ' channel title is currently ' + r['status'])
+
+        # Provide basic information to viewers for specific commands
+        elif cmd == "raffle":
+            message = "This is an example bot, replace this text with your raffle text."
+            c.privmsg(self.channel, message)
+        elif cmd == "schedule":
+            message = "This is an example bot, replace this text with your schedule text."
+            c.privmsg(self.channel, message)
+
+        # The command was not recognized
+        else:
+            c.privmsg(self.channel, "Did not understand command: " + cmd)
 
 def main():
-    if len(sys.argv) != 5:
-        print("Usage: twitchbot <username> <client id> <token> <channel>")
-        sys.exit(1)
 
-    username  = sys.argv[1]
-    client_id = sys.argv[2]
-    token     = sys.argv[3]
-    channel   = sys.argv[4]
-    username2 = sys.argv[5]
-    client_id2 = sys.argv[6]
-    token2    = sys.argv[7]
-    channel2 = sys.argv[8]
-    bot = TwitchBot(username, client_id, token, channel)
-    bot.start()
+    user1 = []
+    user2 = []
+    bots = []
+
+    bots.append(TwitchBot("bloodyplayer415", lambda channelID, message: user1.append(message)))
+    def onMessage(channelID, message):
+        user2.append(message)
+        print(user1, user2)
+
+    bots.append(TwitchBot("animelover231", onMessage))
+    for bot in bots:
+        t = Thread(target=bot.start, args=())
+        t.start()
+
+    print(user1)
 
 if __name__ == "__main__":
     main()
